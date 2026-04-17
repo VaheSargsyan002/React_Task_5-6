2aecb1b0ee7951b059757278ebdb63fc09dbe424
Vahe Sargsyan <vahe.sargsyan.techx@gmail.com>
2026-04-17 17:59:49 +0400

    Added to main

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import "./App.css";
-import Header from "./components/Header";
-import PreferenceChecklist from "./components/PreferenceChecklist";
-import ProfileCard from "./components/ProfileCard";
-import QuoteBox from "./components/QuoteBox";
-import QuickTaskCard from "./components/QuickTaskCard";
-import ReactionPanel from "./components/ReactionPanel";
-import TaskList from "./components/TaskList";
+// import "./App.css";
+// import Header from "./components/Header";
+// import PreferenceChecklist from "./components/PreferenceChecklist";
+// import ProfileCard from "./components/ProfileCard";
+// import QuoteBox from "./components/QuoteBox";
+// import QuickTaskCard from "./components/QuickTaskCard";
+// import ReactionPanel from "./components/ReactionPanel";
+// import TaskList from "./components/TaskList";
 
-function App() {
-  return (
-    <main className="app-shell">
-      <Header />
-      <ProfileCard firstName="Vahe" lastName="Sargsyan" role="student" />
-      <QuoteBox />
-      <QuickTaskCard />
-      <TaskList />
-      <PreferenceChecklist />
-      <ReactionPanel />
-    </main>
-  );
-}
+// function App() {
+//   return (
+//     <main className="app-shell">
+//       <Header />
+//       <ProfileCard firstName="Vahe" lastName="Sargsyan" role="student" />
+//       <QuoteBox />
+//       <QuickTaskCard />
+//       <TaskList />
+//       <PreferenceChecklist />
+//       <ReactionPanel />
+//     </main>
+//   );
+// }
+
+// export default App;
+
+import React from "react";
+
+const App = () => {
+  return <div></div>;
+};
 
 export default App;
